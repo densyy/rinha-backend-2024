5a491d87083312fe79cb5e28d71d2bd6819d49de
Densyy <densyy@hotmail.com>
2024-02-24 19:36:34 -0300

    add /configs/index

---
+++ configs/index.js
@@ -0,0 +1,11 @@
+/* ---- Requires ---- */
+
+const middlewares = require('./middlewares')
+const routes = require('./routes')
+
+/* ---- Methods ---- */
+
+module.exports = {
+  middlewares,
+  routes
+}
