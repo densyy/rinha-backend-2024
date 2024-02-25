a718e330fa1f51981e156fc4ffb8332aac5409b5
Densyy <densyy@hotmail.com>
2024-02-25 00:37:41 -0300

    refatora /bin/http

--- bin/http.js
+++ bin/http.js
@@ -1,7 +1,7 @@
 /* ---- Requires ---- */
 
-const app = require('../app')
 const http = require('http')
+const helperRoutes = require('../helpers/routes')
 
 /* ---- Constants ---- */
 
@@ -9,37 +9,8 @@ const PORT = process.env.PORT
 
 /* ---- Server ---- */
 
-app.set('port', PORT)
-const server = http.createServer(app)
+const server = http.createServer(helperRoutes)
 
-server.listen(PORT)
-server.on('error', onError)
-server.on('listening', onListening)
-
-/* ---- Events ---- */
-
-function onError (error) {
-  if (error.syscall !== 'listen') throw error
-
-  if (error.code === 'EACCES') {
-    console.error(PORT, 'sem permissão')
-    process.exit(1)
-  }
-
-  if (error.code === 'EADDRINUSE') {
-    console.error(PORT, 'já está em uso')
-    process.exit(1)
-  }
-
-  throw error
-}
-
-function onListening () {
-  console.info('# Servidor Iniciado!')
-  console.info('# Escutando na porta', PORT, '...')
-}
-
-// App Error Handler
-process.on('unhandledRejection', (error) => {
-  console.log('Motivo:', error)
+server.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`)
 })
