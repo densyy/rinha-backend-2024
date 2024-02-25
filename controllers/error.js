44242fed7d776579a10daef3da52fc623a801f29
Densyy <densyy@hotmail.com>
2024-02-25 00:37:51 -0300

    add controllers/erro

---
+++ controllers/error.js
@@ -0,0 +1,11 @@
+/* ---- Requires ---- */
+
+const helperResponse = require('../helpers/response')
+
+/* ---- Methods ---- */
+
+function route (_req, res) {
+  return helperResponse.simpleError(res, 404, 'Rota não existe')
+}
+
+module.exports = route
