86773d690ef82cce30ce86963b41a7c68411cb27
Densyy <densyy@hotmail.com>
2024-02-28 04:52:01 -0300

    implementa metodo parseJsonPost em /helpers/routes

--- helpers/routes.js
+++ helpers/routes.js
@@ -1,13 +1,47 @@
 /* ---- Requires ---- */
 
 const controllerIndex = require('../controllers/index')
+const controllerTransacoes = require('../controllers/transacoes')
 const controllerError = require('../controllers/error')
 
 /* ---- Methods ---- */
 
 const routes = (req, res) => {
-  if (req.method === 'GET' && req.url === '/') return controllerIndex(req, res)
-  else return controllerError(req, res)
+  if (req.method === 'GET') return methodGet(req, res)
+  if (req.method === 'POST') return parseJsonPost(req, res)
+  return controllerError(req, res)
+}
+
+//
+// GET
+//
+
+function methodGet (req, res) {
+  if (req.url === '/') return controllerIndex(req, res)
+  return controllerError(req, res)
+}
+
+//
+// POST
+//
+
+function parseJsonPost (req, res) {
+  let body = ''
+  req.on('data', chunk => { body += chunk.toString() })
+  req.on('end', () => {
+    try {
+      req.body = JSON.parse(body)
+    } catch (error) {
+      return controllerError(req, res)
+    }
+
+    return methodPost(req, res)
+  })
+}
+
+function methodPost (req, res) {
+  if (req.url.match(/^\/clientes\/[1-5]\/transacoes$/)) return controllerTransacoes(req, res)
+  return controllerError(req, res)
 }
 
 module.exports = routes
