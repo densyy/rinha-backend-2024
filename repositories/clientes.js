96fe7faebadd4d262d0763dccd0fa862204115b8
Densyy <densyy@hotmail.com>
2024-03-01 06:13:49 -0300

    adiciona /repositories/clientes

---
+++ repositories/clientes.js
@@ -0,0 +1,24 @@
+/* ---- Requires ---- */
+
+const pool = require('./postgres')
+
+/* ---- Methods ---- */
+
+async function receberPorId (idCliente) {
+  const parametros = [idCliente]
+  const sql = 'SELECT * FROM clientes WHERE id=$1'
+  const result = await pool.query(sql, parametros)
+  return result?.rows[0]
+}
+
+async function atualizarSaldo (idCliente, dados) {
+  const valor = (dados.tipo === 'd') ? dados.valor * -1 : dados.valor
+  const parametros = [valor, idCliente]
+  const sql = 'UPDATE clientes SET saldo = saldo + $1 WHERE id = $2'
+  await pool.query(sql, parametros)
+}
+
+module.exports = {
+  receberPorId,
+  atualizarSaldo
+}
