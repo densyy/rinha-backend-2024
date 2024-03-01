30d4507b815734a2f763fb3e70e39a5928832ff6
Densyy <densyy@hotmail.com>
2024-03-01 05:11:04 -0300

    adiciona /repositories/postgres

---
+++ repositories/postgres.js
@@ -0,0 +1,17 @@
+/* ---- Requires ---- */
+
+const { Pool } = require('pg')
+
+/* ---- Configs ---- */
+
+const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DB,
+  max: 20,
+  idleTimeoutMillis: 30000
+})
+
+module.exports = pool
