a2dc991d0c0d59b70dba6f684c225d3315b28062
Densyy <densyy@hotmail.com>
2024-03-03 05:41:38 -0300

    otimiza /repositories/transacoes

--- repositories/transacoes.js
+++ repositories/transacoes.js
@@ -8,6 +8,7 @@ async function adicionar (idCliente, dados) {
   const parametros = [idCliente, dados.valor, dados.tipo, dados.descricao]
   const sql = 'INSERT INTO transacoes (cliente_id, valor, tipo, descricao) VALUES ($1, $2, $3, $4)'
   await pool.query(sql, parametros)
+  return true
 }
 
 async function receberHistorico (idCliente) {
