fd3d5577d4161ecf372707426a9e6b55c8ff9f72
Densyy <densyy@hotmail.com>
2024-03-03 03:43:29 -0300

    corrige resposta em /controllers/error

--- controllers/error.js
+++ controllers/error.js
@@ -5,7 +5,7 @@ const helperResponse = require('../helpers/response')
 /* ---- Methods ---- */
 
 function route (_req, res) {
-  return helperResponse.simpleError(res, 404, 'Rota não existe')
+  return helperResponse.simpleError(res, 404, { message: 'Rota não existe' })
 }
 
 module.exports = route
