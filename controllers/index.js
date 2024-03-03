498ef1ccce014674c3de06cf7314d042fe8a8996
Densyy <densyy@hotmail.com>
2024-03-03 03:43:34 -0300

    corrige resposta em /controllers/index

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,7 @@ const helperResponse = require('../helpers/response')
 /* ---- Methods ---- */
 
 function route (_req, res) {
-  return helperResponse.success(res, 'Bem vindo à Api Rinha de Backend')
+  return helperResponse.success(res, { message: 'Bem vindo à Api Rinha de Backend' })
 }
 
 module.exports = route
