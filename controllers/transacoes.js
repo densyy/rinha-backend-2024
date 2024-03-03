611d2e34ca2934a9d42c96941f6d73597e48ada8
Densyy <densyy@hotmail.com>
2024-03-03 09:00:30 -0300

    atualiza /controllers/transacoes pro modelo antigo

--- controllers/transacoes.js
+++ controllers/transacoes.js
@@ -2,7 +2,8 @@
 
 const helperResponse = require('../helpers/response')
 const validationTransacoes = require('../validations/transacoes')
-const repositoryUnico = require('../repositories/unico')
+const repositoryClientes = require('../repositories/clientes')
+const repositoryTransacoes = require('../repositories/transacoes')
 
 /* ---- Methods ---- */
 
@@ -12,15 +13,42 @@ async function route (req, res) {
     const validacaoOk = validationTransacoes(req.body)
     if (!validacaoOk) return helperResponse.simpleError(res, 422, { message: 'Há erros de validação' })
 
-    // step 2 - update db
+    // step 2 - get id
     const idCliente = req.url.split('/')[2]
-    const resultado = await repositoryUnico.adicionarTransacao(idCliente, req.body)
-    if (!resultado) return helperResponse.simpleError(res, 422, { message: 'Transação inconsistente' })
+    const cliente = await repositoryClientes.receberPorId(idCliente)
 
-    helperResponse.success(res, resultado)
+    // step 3 - check saldo ok
+    const limiteOk = limiteValido(cliente, req.body)
+    if (!limiteOk) return helperResponse.simpleError(res, 422, { message: 'Transação inconsistente' })
+
+    // step 4 - return data
+    const retorno = formatarRetorno(cliente, req.body)
+    helperResponse.success(res, retorno)
+
+    // step 5 - update db
+    const saldoOk = await repositoryClientes.atualizarSaldo(cliente.id, req.body)
+    if (!saldoOk) return helperResponse.simpleError(res, 422, { message: 'Transação inconsistente' })
+    await repositoryTransacoes.adicionar(cliente.id, req.body)
   } catch (_error) {
     return helperResponse.serverError(res)
   }
+}
+
+function limiteValido (cliente, body) {
+  if (body.tipo === 'c') return true
+
+  const saldoFuturo = cliente.saldo - body.valor
+  const prejuizoMaximo = cliente.limite * -1
+  if (saldoFuturo >= prejuizoMaximo) return true
+
+  return false
+}
+
+function formatarRetorno (cliente, body) {
+  const tipo = (body.tipo === 'd') ? -1 : 1
+  const limite = cliente.limite
+  const saldo = cliente.saldo + (body.valor * tipo)
+  return { limite, saldo }
 }
 
 module.exports = route
