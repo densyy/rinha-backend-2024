e2473b7c3beae6a96b4fb13036de93fc04beb4c3
Densyy <densyy@hotmail.com>
2024-03-03 08:01:11 -0300

    refatora /controllers/transacoes

--- controllers/transacoes.js
+++ controllers/transacoes.js
@@ -2,7 +2,6 @@
 
 const helperResponse = require('../helpers/response')
 const validationTransacoes = require('../validations/transacoes')
-const repositoryClientes = require('../repositories/clientes')
 const repositoryUnico = require('../repositories/unico')
 
 /* ---- Methods ---- */
@@ -13,31 +12,15 @@ async function route (req, res) {
     const validacaoOk = validationTransacoes(req.body)
     if (!validacaoOk) return helperResponse.simpleError(res, 422, { message: 'Há erros de validação' })
 
-    // step 2 - get id
+    // step 2 - update db
     const idCliente = req.url.split('/')[2]
-    const cliente = await repositoryClientes.receberPorId(idCliente)
-
-    // step 3 - check saldo ok
-    const limiteOk = limiteValido(cliente, req.body)
-    if (!limiteOk) return helperResponse.simpleError(res, 422, { message: 'Transação inconsistente' })
-
-    // step 4 - update db
     const resultado = await repositoryUnico.adicionarTransacao(idCliente, req.body)
+    if (!resultado) return helperResponse.simpleError(res, 422, { message: 'Transação inconsistente' })
 
     helperResponse.success(res, resultado)
   } catch (_error) {
     return helperResponse.serverError(res)
   }
-}
-
-function limiteValido (cliente, body) {
-  if (body.tipo === 'c') return true
-
-  const saldoFuturo = cliente.saldo - body.valor
-  const prejuizoMaximo = cliente.limite * -1
-  if (saldoFuturo >= prejuizoMaximo) return true
-
-  return false
 }
 
 module.exports = route
