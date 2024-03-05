97c9fe4c9bc56f8c2981712dcbe7264103879cb7
Densyy <densyy@hotmail.com>
2024-03-05 05:50:36 -0300

    implementa prepare sql em /repositories/transacoes

--- repositories/transacoes.js
+++ repositories/transacoes.js
@@ -6,14 +6,14 @@ const pool = require('./postgres')
 
 async function adicionar (idCliente, dados) {
   const parametros = [idCliente, dados.valor, dados.tipo, dados.descricao]
-  const sql = 'INSERT INTO transacoes (cliente_id, valor, tipo, descricao) VALUES ($1, $2, $3, $4)'
+  const sql = 'EXECUTE transacoes_adicionar($1, $2, $3, $4);'
   await pool.query(sql, parametros)
   return true
 }
 
 async function receberHistorico (idCliente) {
   const parametros = [idCliente]
-  const sql = 'SELECT * FROM transacoes WHERE cliente_id=$1 ORDER BY data_registro DESC LIMIT 10'
+  const sql = 'EXECUTE transacoes_receber_historico($1);'
   const result = await pool.query(sql, parametros)
   return result?.rows
 }
