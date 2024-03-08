6aee4923eb45ce8f7434329016d91f19cb26b433
Densyy <densyy@hotmail.com>
2024-03-08 09:33:16 -0300

    atualiza /docker/test

--- docker/test.js
+++ docker/test.js
@@ -1,3 +1,6 @@
+// para testar no docker
+// docker run --rm -ti --network host -v $PWD/test.js:/tmp/test.js grafana/k6 run -i 5000 --vus 4 /tmp/test.js
+
 import http from 'k6/http'
 
 export default function () {
