a7593fdde196323cfb437ba012673380a107cc60
Densyy <densyy@hotmail.com>
2024-03-08 10:56:59 -0300

    refatora /controllers/transacoes com funcao debito e credito

--- controllers/transacoes.js
+++ controllers/transacoes.js
@@ -2,8 +2,7 @@
 
 const helperResponse = require('../helpers/response')
 const validationTransacoes = require('../validations/transacoes')
-const repositoryClientes = require('../repositories/clientes')
-const repositoryTransacoes = require('../repositories/transacoes')
+const repositoryUnico = require('../repositories/unico')
 
 /* ---- Methods ---- */
 
@@ -13,42 +12,17 @@ async function route (req, res) {
     const validacaoOk = validationTransacoes(req.body)
     if (!validacaoOk) return helperResponse.simpleError(res, 422)
 
-    // step 2 - get id
     const idCliente = req.url.split('/')[2]
-    const cliente = await repositoryClientes.receberPorId(idCliente)
+    let resultado = ''
 
-    // step 3 - check saldo ok
-    const limiteOk = limiteValido(cliente, req.body)
-    if (!limiteOk) return helperResponse.simpleError(res, 422)
+    if (req.body.tipo === 'c') resultado = await repositoryUnico.credito(idCliente, req.body)
+    else resultado = await repositoryUnico.debito(idCliente, req.body)
 
-    // step 4 - update db
-    const saldoOk = await repositoryClientes.atualizarSaldo(cliente.id, req.body)
-    if (!saldoOk) return helperResponse.simpleError(res, 422)
-    await repositoryTransacoes.adicionar(cliente.id, req.body)
-
-    // step 5 - return data
-    const retorno = formatarRetorno(cliente, req.body)
-    helperResponse.success(res, retorno)
+    if (resultado === false) return helperResponse.simpleError(res, 422)
+    return helperResponse.success(res, resultado)
   } catch (_error) {
     return helperResponse.serverError(res)
   }
-}
-
-function limiteValido (cliente, body) {
-  if (body.tipo === 'c') return true
-
-  const saldoFuturo = cliente.saldo - body.valor
-  const prejuizoMaximo = cliente.limite * -1
-  if (saldoFuturo >= prejuizoMaximo) return true
-
-  return false
-}
-
-function formatarRetorno (cliente, body) {
-  const tipo = (body.tipo === 'd') ? -1 : 1
-  const limite = cliente.limite
-  const saldo = cliente.saldo + (body.valor * tipo)
-  return { limite, saldo }
 }
 
 module.exports = route
