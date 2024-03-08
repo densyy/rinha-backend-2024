66ca0092452a933681084a0b8360c8721346e91a
Densyy <densyy@hotmail.com>
2024-03-08 10:57:06 -0300

    adiciona /repositories/unico

---
+++ repositories/unico.js
@@ -0,0 +1,34 @@
+/* ---- Requires ---- */
+
+const pool = require('./postgres')
+
+/* ---- Methods ---- */
+
+async function debito (idCliente, dados) {
+  try {
+    const parametros = [idCliente, dados.valor, dados.descricao]
+    const sql = 'SELECT debito($1, $2, $3)'
+    const resultado = await pool.query(sql, parametros)
+
+    if (resultado?.rows[0]?.debito?.error) return false
+    return resultado?.rows[0]?.debito
+  } catch (error) {
+    return false
+  }
+}
+
+async function credito (idCliente, dados) {
+  try {
+    const parametros = [idCliente, dados.valor, dados.descricao]
+    const sql = 'SELECT credito($1, $2, $3)'
+    const resultado = await pool.query(sql, parametros)
+    return resultado?.rows[0]?.credito
+  } catch (error) {
+    return false
+  }
+}
+
+module.exports = {
+  debito,
+  credito
+}
